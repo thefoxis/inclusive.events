568a179e69c04ed4c94ca4777306202c2d14c295
Karolina Szczur <hi@thefox.is>
2015-11-04 11:17:46 +0100

    add mailchimp, flexboxgrid and normalize

---
+++ Gulpfile.js
@@ -0,0 +1,40 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+var harp        = require('harp');
+
+/**
+ * Serve the Harp Site from the src directory
+ */
+gulp.task('serve', function () {
+  harp.server(__dirname + '/_harp', {
+    port: 9000
+  }, function () {
+    browserSync({
+      proxy: "localhost:9000",
+      open: false,
+      /* Hide the notification. It gets annoying */
+      notify: {
+        styles: ['opacity: 0', 'position: absolute']
+      }
+    });
+    /**
+     * Watch for scss changes, tell BrowserSync to refresh main.css
+     */
+    gulp.watch("_harp/public/css/*.styl", function () {
+      reload("main.css", {stream: true});
+    });
+    /**
+     * Watch for all other changes, reload the whole page
+     */
+    gulp.watch(["_harp/public/*.jade", "_harp/public/*.json"], function () {
+      reload();
+    });
+  })
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the harp site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['serve']);
